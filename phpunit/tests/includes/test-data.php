dc0a6305aa9def3930d37fdad1e3affdce42b7db
Sunny Johal <sunny@titaniumthemes.com>
2021-05-30 08:29:24 +0100

    feat: created data posttype for font controls

--- phpunit/tests/includes/test-data.php
+++ phpunit/tests/includes/test-data.php
@@ -5,7 +5,7 @@
  * Test posttype registration and any CRUD
  * functionality.
  *
- * @package Easy_Custom_Sidebars
+ * @package easy-google-fonts
  */
 
 use EGF\Data as Data;
@@ -14,4 +14,108 @@ use EGF\Data as Data;
  * Class ECS_Test_Data
  */
 class EGF_Test_Data extends WP_UnitTestCase {
+	/**
+	 * ID of font control instance.
+	 *
+	 * @var int $post_id
+	 */
+	protected static $post_id;
+
+	/**
+	 * ID of temp user created for tests.
+	 *
+	 * @var int $user_id
+	 */
+	protected static $user_id;
+
+	/**
+	 * Setup before any class tests are run.
+	 *
+	 * @param object $factory Factory obj for generating WordPress fixtures.
+	 */
+	public static function wpSetUpBeforeClass( $factory ) {
+		self::$post_id = $factory->post->create( [ 'post_type' => 'tt_font_control' ] );
+	}
+
+	/**
+	 * Clean up after all class tests are run.
+	 */
+	public static function wpTearDownAfterClass() {
+		wp_delete_post( self::$post_id, true );
+	}
+
+	/**
+	 * Test Posttype Creation for font controls.
+	 */
+	public function test_register_post_type_for_font_controls() {
+		$this->assertTrue(
+			post_type_exists( 'tt_font_control' ),
+			'Failed to register the post type.'
+		);
+	}
+
+	/**
+	 * Test Posttype supported features.
+	 */
+	public function test_supported_features() {
+		$this->assertTrue(
+			post_type_supports( 'tt_font_control', 'title' ),
+			'Font controls needs to opt in for title support.'
+		);
+
+		$this->assertTrue(
+			post_type_supports( 'tt_font_control', 'custom-fields' ),
+			'Font control post type needs to opt in for custom field support.'
+		);
+	}
+
+	/**
+	 * Test Get Font Control ID
+	 */
+	public function test_get_font_control_id() {
+		$font_control_id = Data\get_font_control_id( self::$post_id );
+
+		$this->assertEquals(
+			$font_control_id,
+			'egf-font-control-' . self::$post_id
+		);
+	}
+
+	/**
+	 * Test Get Font Control Description
+	 */
+	public function test_get_font_control_description() {
+		$description = 'This is an example font control description';
+		update_post_meta( self::$post_id, 'control_description', $description );
+
+		// Valid font control id.
+		$this->assertEquals(
+			Data\get_font_control_description( self::$post_id ),
+			$description
+		);
+
+		// Invalid font control id.
+		$this->assertEquals(
+			Data\get_font_control_description( 0 ),
+			false
+		);
+	}
+
+	/**
+	 * Test Get Font Control Selectors
+	 */
+	public function test_get_font_control_selectors() {
+		$selectors = [ '.test-class .with-selector', '#random-id' ];
+
+		// Ensure arr when no value is passed.
+		$this->assertIsArray( Data\get_font_control_selectors( self::$post_id ) );
+
+		update_post_meta( self::$post_id, 'control_selectors', $selectors );
+
+		$this->assertEquals(
+			Data\get_font_control_selectors( self::$post_id ),
+			$selectors
+		);
+	}
 }
+
--- src/includes/data.php
+++ src/includes/data.php
@@ -11,3 +11,132 @@
  */
 
 namespace EGF\Data;
+
+/**
+ * Register Posttype
+ *
+ * Registers a new post type to hold any custom
+ * font controls created.
+ *
+ * @since 2.0.0
+ */
+function register_post_type_for_font_controls() {
+	register_post_type(
+		'tt_font_control',
+		[
+			'labels'                => [
+				'name'          => __( 'Font Controls', 'easy-google-fonts' ),
+				'singular_name' => __( 'Font Control', 'easy-google-fonts' ),
+			],
+			'public'                => false,
+			'hierarchical'          => false,
+			'rewrite'               => false,
+			'delete_with_user'      => false,
+			'query_var'             => false,
+			'show_in_rest'          => true,
+			'rest_base'             => 'easy-google-fonts',
+			'rest_controller_class' => 'WP_REST_Posts_Controller',
+			'supports'              => [
+				'custom-fields',
+				'title',
+			],
+		]
+	);
+}
+add_action( 'init', __NAMESPACE__ . '\\register_post_type_for_font_controls' );
+
+/**
+ * Register Meta
+ *
+ * Register metadata to hold properties for
+ * each font control.
+ *
+ * @since 2.0.0
+ */
+function register_metadata_for_font_controls() {
+	register_meta(
+		'post',
+		'force_styles',
+		[
+			'object_subtype'    => 'tt_font_control',
+			'type'              => 'boolean',
+			'description'       => __( 'If enabled each style will have the !important CSS rule applied to it.', 'easy-google-fonts' ),
+			'single'            => true,
+			'sanitize_callback' => 'rest_sanitize_boolean',
+			'show_in_rest'      => true,
+		]
+	);
+
+	register_meta(
+		'post',
+		'control_selectors',
+		[
+			'object_subtype' => 'tt_font_control',
+			'type'           => 'array',
+			'description'    => __( "All of the CSS selectors that this font control's styles should be applied to.", 'easy-google-fonts' ),
+			'single'         => true,
+			'show_in_rest'   => true,
+		]
+	);
+
+	register_meta(
+		'post',
+		'control_description',
+		[
+			'object_subtype'    => 'tt_font_control',
+			'type'              => 'string',
+			'description'       => __( 'Description of the font control, displayed in the customizer.', 'easy-google-fonts' ),
+			'single'            => true,
+			'sanitize_callback' => 'sanitize_text_field',
+			'show_in_rest'      => true,
+		]
+	);
+}
+add_action( 'init', __NAMESPACE__ . '\\register_metadata_for_font_controls' );
+
+/**
+ * Get Font Control ID
+ *
+ * Gets the unique identifier for
+ * each font control.
+ *
+ * @param int $post_id ID of a 'tt_font_control' post.
+ *
+ * @return string Custom font control id.
+ */
+function get_font_control_id( $post_id ) {
+	return apply_filters( 'egf_font_control_id', "egf-font-control-{$post_id}", $post_id );
+}
+
+/**
+ * Get Font Control Description
+ *
+ * Gets the description text that will be
+ * displayed in the Widgets interface.
+ *
+ * @param int $post_id ID of a 'tt_font_control' post.
+ */
+function get_font_control_description( $post_id ) {
+	return apply_filters(
+		'egf_font_control_description',
+		get_post_meta( $post_id, 'control_description', true ),
+		$post_id
+	);
+}
+
+/**
+ * Get Font Control Selectors
+ *
+ * Gets the css selectors that will be
+ * controlled by the font control id.
+ *
+ * @param int $post_id ID of a 'tt_font_control' post.
+ */
+function get_font_control_selectors( $post_id ) {
+	$selectors = get_post_meta( $post_id, 'control_selectors', true );
+	return empty( $selectors ) ? [] : apply_filters(
+		'egf_font_control_selectors',
+		$selectors,
+		$post_id
+	);
+}
